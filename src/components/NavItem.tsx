c33f2d3ffad47fe458973184a3fcefff8bc45335
g0657 <g065700@naver.com>
2025-05-22 17:25:38 +0900

    #10: [feature] Next Auth 적용

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,6 +1,10 @@
 import Link from 'next/link';
+import { useSession, signIn, signOut } from 'next-auth/react';
 
 const NavItem = ({ mobile }: { mobile?: boolean }) => {
+  const { data: session, status } = useSession();
+  console.log({ session }, status);
+
   return (
     <ul
       className={`w-full text-md flex justify-center items-center gap-4 ${mobile && 'flex-col h-full py-4'}`}
@@ -11,11 +15,13 @@ const NavItem = ({ mobile }: { mobile?: boolean }) => {
       <li className="py-2 text-center cursor-pointer">
         <Link href="/user">User</Link>
       </li>
+
       <li className="py-2 text-center cursor-pointer">
-        <button>Signout</button>
-      </li>
-      <li className="py-2 text-center cursor-pointer">
-        <button>Signin</button>
+        {session?.user ? (
+          <button onClick={() => signOut()}>SignOut</button>
+        ) : (
+          <button onClick={() => signIn()}>SignIn</button>
+        )}
       </li>
     </ul>
   );
---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,55 @@
+import NextAuth, { NextAuthOptions } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@/prisma/client';
+
+const prisma = new PrismaClient();
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials, req) {
+        const user = {
+          id: '1',
+          name: 'J Smith',
+          email: 'jsmith@example.com',
+        };
+
+        if (user) {
+          return user;
+        } else {
+          return null;
+        }
+        // const res = await fetch('/your/endpoint', {
+        //   method: 'POST',
+        //   body: JSON.stringify(credentials),
+        //   headers: { 'Content-Type': 'application/json' },
+        // });
+        // const user = await res.json();
+        //
+        // // If no error and we have user data, return it
+        // if (res.ok && user) {
+        //   return user;
+        // }
+        // // Return null if user data could not be retrieved
+        // return null;
+      },
+    }),
+  ],
+  session: {
+    strategy: 'jwt',
+  },
+};
+
+export default NextAuth(authOptions);
