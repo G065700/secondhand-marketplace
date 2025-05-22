9c8f0047b2e8cd22d17e9aa9a0f05478682d3af1
g0657 <g065700@naver.com>
2025-05-22 17:25:38 +0900

    #10: [feature] Next Auth 도입 및 인증/인가 기능 구현

---
+++ src/components/Button.tsx
@@ -0,0 +1,50 @@
+import { FC, MouseEvent } from 'react';
+import { IconType } from 'react-icons';
+
+interface ButtonProps {
+  label: string;
+  onClick?: (e: MouseEvent<HTMLButtonElement>) => void;
+  disabled?: boolean;
+  outline?: boolean;
+  small?: boolean;
+  icon?: IconType;
+}
+
+const Button: FC<ButtonProps> = ({
+  label,
+  onClick,
+  disabled,
+  outline,
+  small,
+  icon: Icon,
+}) => {
+  return (
+    <button
+      type="submit"
+      disabled={disabled}
+      onClick={onClick}
+      className={`
+        relative
+        cursor-pointer
+        disabled:opacity-70
+        disabled:cursor-not-allowed
+        rounded-lg
+        hover:opacity-80
+        transition
+        w-full
+        ${outline ? 'bg-white' : 'bg-teal-500'}
+        ${outline ? 'border-black' : 'border-teal-500'}
+        ${outline ? 'text-black' : 'text-white'}
+        ${small ? 'text-sm' : 'text-md'}
+        ${small ? 'py-1' : 'py-3'}
+        ${small ? 'font-light' : 'font-semibold'}
+        ${small ? 'border' : 'border-2'}
+      `}
+    >
+      {Icon && <Icon size={24} className="absolute left-4 top-3" />}
+      {label}
+    </button>
+  );
+};
+
+export default Button;
---
+++ src/middleware.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { getToken } from 'next-auth/jwt';
+
+export { default } from 'next-auth/middleware';
+
+export async function middleware(req: NextRequest) {
+  const session = await getToken({ req });
+  const pathname = req.nextUrl.pathname;
+
+  // 로그인된 사용자만 User/Admin 관련 페이지 접근 가능
+  if (
+    (pathname.startsWith('/user') || pathname.startsWith('/admin')) &&
+    !session
+  ) {
+    return NextResponse.redirect(new URL('/auth/sign-in', req.url));
+  }
+
+  // Admin 사용자만 Admin 관련 페이지 접근 가능
+  if (pathname.startsWith('/admin') && session?.role !== 'Admin') {
+    return NextResponse.redirect(new URL('/', req.url));
+  }
+
+  // 로그인된 사용자는 로그인, 회원가입 페이지 접근 불가
+  if (pathname.startsWith('/auth') && session) {
+    return NextResponse.redirect(new URL('/', req.url));
+  }
+
+  return NextResponse.next();
+}
