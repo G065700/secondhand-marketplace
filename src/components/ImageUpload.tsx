c5b29c8f2dfaf3cb2fe72d192d153f3c5769d21e
g0657 <g065700@naver.com>
2025-05-28 14:31:16 +0900

    #12: [feature] 상품 생성 구현

---
+++ src/components/ImageUpload.tsx
@@ -0,0 +1,60 @@
+import { FC } from 'react';
+import { CldUploadWidget } from 'next-cloudinary';
+import { TbPhotoPlus } from 'react-icons/tb';
+import Image from 'next/image';
+
+interface ImageUploadProps {
+  value: string;
+  onChange: (value: string) => void;
+}
+
+const ImageUpload: FC<ImageUploadProps> = ({ value, onChange }) => {
+  const handleUpload = (result: any) => {
+    console.log('result', result);
+    onChange(result.info.secure_url);
+  };
+
+  const uploadPreset = process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET;
+
+  return (
+    <CldUploadWidget
+      onSuccess={handleUpload}
+      uploadPreset={uploadPreset}
+      options={{ maxFiles: 1 }}
+    >
+      {({ open }) => {
+        return (
+          <div
+            onClick={() => open?.()}
+            className="
+              relative
+              flex flex-col
+              justify-center items-center
+              gap-4
+              p-20
+              transition
+              border-2 border-dashed border-neutral-300
+              cursor-pointer
+              hover:opacity-70
+              text-neutral-300
+            "
+          >
+            <TbPhotoPlus size={50} />
+            {value && (
+              <div className="absolute inset-0 w-full h-full">
+                <Image
+                  fill
+                  style={{ objectFit: 'cover' }}
+                  src={value}
+                  alt="uploaded product image"
+                />
+              </div>
+            )}
+          </div>
+        );
+      }}
+    </CldUploadWidget>
+  );
+};
+
+export default ImageUpload;
